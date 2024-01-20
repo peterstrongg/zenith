cf6a02f6ec62e42c26fa3376566839a763884966
peterstrongg <peterstrongdev@outlook.com>
2024-01-20 17:43:52 -0500

    Minor changes

--- analysis_scripts/firefox/firefox.py
+++ analysis_scripts/firefox/firefox.py
@@ -3,3 +3,6 @@ import sqlite3
 class Firefox:
     def __init__(self):
         pass
+
+if __name__ == "__main__":
+    print("Instantiated")
--- analysis_scripts/wizard.py
+++ analysis_scripts/wizard.py
@@ -1,5 +1,35 @@
 from firefox import firefox
 
-if __name__ == "__main__":
+def print_banner():
+    print("Welcome to the Zenith wizard!")
+    print("Select a browser to analyze to get started:")
+    print("""
+    (F)irefox
+    (C)hrome
+    (E)dge
+    E(X)it
+    """)
+    choice = input("> ")
+    return choice
+
+def analyze_firefox():
+    ffox = firefox.Firefox()
+
+def analyze_chrome():
     pass
-    
+
+def analyze_edge():
+    pass
+
+if __name__ == "__main__":
+    while True:
+        choice = print_banner()
+        if choice == "F":
+            analyze_firefox()
+        elif choice == "C":
+            analyze_chrome()
+        elif choice == "E":
+            analyze_edge()
+        else:
+            print("ERROR: Invalid option selected")
+
