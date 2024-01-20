4abc6f15728a6af16e2800f96fcc7a50a793491e
peterstrongg <peterstrongdev@outlook.com>
2024-01-20 14:42:41 -0500

    Started analysis for firefox

---
+++ analysis_scripts/firefox/firefox.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+class Firefox:
+    def __init__(self):
+        pass
---
+++ analysis_scripts/wizard.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    pass
+    
+
+from firefox import firefox
+
+x = firefox.Firefox()
